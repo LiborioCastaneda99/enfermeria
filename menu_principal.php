b179673b50fb88cec2dec62cc61ef219875a8a49
LiborioCastaneda99 <ldcastaneda06@misena.edu.co>
2022-03-28 20:54:12 -0500

    first commit

---
+++ menu_principal.php
@@ -0,0 +1,78 @@
+<header>
+    <div class="header-area ">
+        <div class="header-top_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-md-6 ">
+                        <div class="social_media_links">
+                            <a href="#">
+                                <i class="fa fa-linkedin"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa fa-google-plus"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-md-6">
+                        <div class="short_contact_list">
+                            <ul>
+                                <li><a href="mailto:mpescano@uninorte.edu.co"> <i class="fa fa-envelope"></i> mpescano@uninorte.edu.co</a></li>
+                                <li><a href="tel:+573046203681"> <i class="fa fa-phone"></i> 3046203681</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="sticky-header" class="main-header-area">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-3 col-lg-2">
+                        <div class="logo">
+                            <a href="index.html">
+                                <img src="img/logo.png" alt="">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-7">
+                        <div class="main-menu  d-none d-lg-block">
+                            <nav>
+                                <ul id="navigation">
+                                    <li><a class="active" href="index.html">Inicio</a></li>
+                                    <li><a href="Department.html">Acerca de</a></li>
+                                    <li><a href="#">DX Enfermería <i class="ti-angle-down"></i></a>
+                                        <ul class="submenu">
+                                            <li><a href="blog.html">blog</a></li>
+                                            <li><a href="single-blog.html">single-blog</a></li>
+                                        </ul>
+                                    </li>
+                                    <!-- <li><a href="#">pages <i class="ti-angle-down"></i></a>
+                                        <ul class="submenu">
+                                            <li><a href="elements.html">elements</a></li>
+                                            <li><a href="about.html">about</a></li>
+                                        </ul>
+                                    </li> -->
+                                    <li><a href="Doctors.html">Enfermeros</a></li>
+                                    <li><a href="contact.html">Contacto</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                        <div class="Appointment">
+                            <div class="book_btn d-none d-lg-block">
+                                <a class="popup-with-form" href="#test-form">Make an Appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
